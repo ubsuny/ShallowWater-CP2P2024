ace480ca8d3318a0383d0e1630b580f9ab25bd63
Mosaddeq Hossain <66480280+Mosaddeq107@users.noreply.github.com>
2024-03-06 12:43:50 -0500

    Update random_numbers.c

--- random_numbers.c
+++ random_numbers.c
@@ -1,1 +1,20 @@
+#include <petscsys.h>
+
+int main(int argc,char **argv)
+{
+  PetscInitialize(&argc,&argv,(char*)0,NULL);
+  PetscRandom    rctx;
+  PetscReal      rval;
+
+  PetscRandomCreate(PETSC_COMM_WORLD,&rctx);
+  PetscRandomSetFromOptions(rctx);
+  
+  // Generate a random number
+  PetscRandomGetValue(rctx,&rval);
+  PetscPrintf(PETSC_COMM_WORLD,"Random Value: %g\n",(double)rval);
+
+  PetscRandomDestroy(&rctx);
+  PetscFinalize();
+  return 0;
+}
 
